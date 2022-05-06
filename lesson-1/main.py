600d3bf573cfc4ef00626c5c0b94fcdf1e79d9ad
darkwingdck <dzakharov3819@gmail.com>
2022-05-06 15:50:19 +0300

    feat: init second task

--- lesson-1/main.py
+++ lesson-1/main.py
@@ -1,5 +1,5 @@
 def request_program():
-    print('Select program:\n1. Number to bit scale')
+    print('Select program:\n1. Number to bit scale\n2. Set operations')
     program_num = int(input('>> '))
     return program_num
 
@@ -11,10 +11,15 @@ def bit_scale():
         number //= 2
     print('Number in bit scale: ' + res)
 
+def set_operations():
+    pass
+
 def main():
     program_num = request_program()
     if program_num == 1:
         bit_scale()
+    elif program_num == 2:
+        set_operations()
 
 if __name__ == '__main__':
     main()
