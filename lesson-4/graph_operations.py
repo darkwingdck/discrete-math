29bb1ec61300ffd0e369b3ae648f7d2bb227050c
darkwingdck <dzakharov3819@gmail.com>
2022-05-27 13:02:02 +0300

    commit

--- lesson-4/graph_operations.py
+++ lesson-4/graph_operations.py
@@ -2,10 +2,10 @@ graph1 = {1: {2, 3, 4}, 2: {1}, 3: {1}, 4: {1}}
 graph2 = {2: {3, 4}, 3: {2}, 4: {2}}
 merged = graph2
 for key, value in graph1.items():
-  if key not in merged:
-    merged[key] = value
-  else:
-    merged[key].update(value)
+    if key not in merged:
+        merged[key] = value
+    else:
+        merged[key].update(value)
 print('***Объединение***')
 print(merged)
 
@@ -15,28 +15,45 @@ graph2 = {4: {5}, 5: {4, 6}, 6: {5}}
 merged = graph1.copy()
 merged[1].update({4})
 for i in graph2:
-  for key, item in merged.items():
-    merged[key].update({i})
+    for key, item in merged.items():
+        merged[key].update({i})
 merged.update(graph2)
 for i in graph1:
-  for j in graph2:
-    merged[j].update({i})
+    for j in graph2:
+        merged[j].update({i})
 print('***Соединение***')
 print(merged)
 
 
-graph1 = {1: {2, 3, 4}, 2: {1}, 3: {1}, 4: {1}}
-vertices = []
-for i in graph1:
-  vertices.append(i)
+graph1 = {1: {2, 3, 4}, 2: {1,3}, 3: {1, 2}, 4: {1}}
+graph2 = {2: {3, 4}, 3: {2}, 4: {2}}
+ans = {}
 for key, value in graph1.items():
-  new_key = set()
-  for j in vertices:
-    if j not in graph1[key] and j != key:
-      new_key.update({j})
-  graph1[key] = new_key
-  if len(graph1[key]) == 0:
-    graph1[key] = {}
+    if key in graph2:
+        a = set()
+        for i in graph1[key]:
+            if i in graph2[key]:
+                a.update({i})
+        if len(a) == 0:
+            ans.update({key: {}})
+        else:
+            ans.update({key: a})
+print('***Пересечение***')
+print(ans)
+
+
+graph1 = {1: {2, 3, 4}, 2: {1}, 3: {1}, 4: {1}}
+vertices =[]
+for i in graph1:
+    vertices.append(i)
+for key, value in graph1.items():
+    new_key = set()
+    for j in vertices:
+        if j not in graph1[key] and j != key:
+            new_key.update({j})
+    graph1[key] = new_key
+    if len(graph1[key])  == 0:
+        graph1[key] = {}
 print('***Дополнение***')
 print(graph1)
 
@@ -45,7 +62,7 @@ graph1 = {1: {2, 3, 4}, 2: {1}, 3: {1}, 4: {1}}
 vertice_to_delete = 4
 edges_to_delete = graph1[vertice_to_delete]
 for key, value in graph1.items():
-  graph1[key].discard(vertice_to_delete)
+    graph1[key].discard(vertice_to_delete)
 del graph1[vertice_to_delete]
 print('***Удаление***')
 print(graph1)
